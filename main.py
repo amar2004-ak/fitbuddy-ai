05c8990c5390706c0699e66e3c7c16c7ab4b033c
amar2004-ak <ak1504008@gmail.com>
2026-03-03 23:23:59 +0530

    Fixed genai import properly

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-from google import genai
+import google.generativeai as genai
 import os
 import logging
 import io
